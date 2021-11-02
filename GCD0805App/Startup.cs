2b9feda6483bb018df5000cd8edee1c26a8a80ce
ThaiTan-git <63284265+ThaiTan-git@users.noreply.github.com>
2021-11-02 16:26:42 +0700

    Begin

---
+++ GCD0805App/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GCD0805App.Startup))]
+namespace GCD0805App
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
